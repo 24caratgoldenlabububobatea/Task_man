eb537db0833ed98c6f60f377abcea0440ecf3025
“24caratgoldenlabububobatea” <jonathanrasmussen2020@gmail.com>
2026-01-14 15:24:53 +0100

    test om at alt funker.

---
+++ process_monitor/process/process_info.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hjelp meg :p" << std::endl;
+    return 0;
+}
+    
---
+++ process_monitor/process/process_info.h
@@ -0,0 +1,7 @@
+#ifndef PROCESS_INFO_H
+#define PROCESS_INFO_H
+
+// Declaration for a small helper used during development.
+void print_help();
+
+#endif // PROCESS_INFO_H
